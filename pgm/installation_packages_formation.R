3bf9b61960ae8fec81ca085fb3670261b7b12a2c
Marylène Henry <marylene.henry@gmail.com>
2019-12-05 10:41:19 +0100

    avant maj exo

---
+++ pgm/installation_packages_formation.R
@@ -0,0 +1,4 @@
+# Scripts d'installation des packages pour la formation
+install.packages(c("lattice","latticeExtra", "copula", "ellipse", "gridBase",
+                   "locfit", "logspline", "mapproj", "maps", "MEMSS",
+                   "mlmRev", "RColorBrewer"))
